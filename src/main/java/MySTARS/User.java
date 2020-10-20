355a2783da0b82b1cc5d115be8c63e89535839b5
Woon Jia Hui <woonjiahui@gmail.com>
2020-10-20 16:46:01 +0800

    User superclass draft

---
+++ src/main/java/MySTARS/User.java
@@ -0,0 +1,34 @@
+package MySTARS;
+
+public abstract class User {
+    private String username;
+    protected AccessLevel accessLevel;
+    protected String password;
+
+    protected User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    protected boolean changePassword(String newPasswrd){
+        //reinput password to ensure correct person changing password
+        password = newPasswrd;
+        return true;
+        //if wrong password, return false
+    }
+
+    protected boolean checkPasswrd(String input){
+        return input.equals(password);
+    }
+
+    protected boolean changeAccessLevel(AccessLevel al){
+        //reinput password
+        accessLevel = al;
+        return true;
+        //else return false
+    }
+
+    protected String getUsername(){
+        return username;
+    }
+}
