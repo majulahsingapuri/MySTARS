d4681fe68d0cdb04c1d21a724c74de064b3a55a7
Bhargav Singapuri <bhsingapuri@gmail.com>
2020-10-22 22:22:45 +0800

    Added abstract View class
    Added abstract view. No implementation of clearScreen as yet.

---
+++ src/main/java/MySTARS/View.java
@@ -0,0 +1,11 @@
+package MySTARS;
+
+public abstract class View {
+    
+    protected abstract void print();
+
+    protected void clearScreen(String directory){
+
+        //TODO for Esther/Nicolette: add clear screen print statements. Refer to Tanay's for guidance
+    }
+}
