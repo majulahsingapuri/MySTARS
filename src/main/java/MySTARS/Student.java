52a055b57fc20fa3d06498f1d4b720d28a25dd60
Woon Jia Hui <woonjiahui@gmail.com>
2020-11-05 20:32:59 +0800

    AUs, clash skeleton
    Add the AU implementation, added skeleton code for checking timetable clashes

--- src/main/java/MySTARS/Student.java
+++ src/main/java/MySTARS/Student.java
@@ -6,10 +6,11 @@ public final class Student extends User {
 
     private static final long serialVersionUID = 77L;
     private String matricNumber;
-    //TODO possibly rename this to something that doesn't clash with Database.Database.COURSES?
-    private HashMap<String, Course> courses = new HashMap<String, Course>(); //<username, course> where course only contains the student's course index
+    //TODO possibly rename this to something that doesn't clash with Database.COURSES?
+    private HashMap<String, Course> courses = new HashMap<String, Course>(); //<courseCode, course> where course only contains the student's course index
     private Gender gender = Gender.PREFER_NOT_TO_SAY;
     private String nationality = "";
+    private int registeredAUs = 0;
 
     public Student(String name, String matricNumber) {
 
@@ -69,12 +70,30 @@ public final class Student extends User {
         return courseInds;
     }
 
-    //TODO is there a better way to do this? return the CourseStatus rather than a boolean to make it more useable?
+    protected int getAU(){
+        return this.registeredAUs;
+    }
+
+    protected void addAU(int au){
+        this.registeredAUs += au;
+    }
+
+    protected void removeAU(int au){
+        this.registeredAUs -= au;
+        //TODO is this needed...? should this method throw an exception instead?
+        //ensure logic is not broken
+        if (this.registeredAUs < 0){
+            this.registeredAUs = 0;
+        }
+    }
+
+    //TODO is there a better way to do this? return the CourseStatus rather than a boolean to make it more usable?
     /*
-    returning true means the course has been added successfully
-    returning false means the student has been added to the waitlist
+    returns the course's new CourseStatus
+    throws exceptions when a timetable clash is found or the course code does not exist
     */
     protected boolean addCourse(String code, String ind) throws Exception{
+        //TODO make sure calling class checks that course is not yet registered nor on the waitlist
         if (clashes(code))
             throw new Exception("Timetable clash!");
         if (!Database.COURSES.containsKey(code))
@@ -92,14 +111,16 @@ public final class Student extends User {
             //TODO Need to modify this to pass in CourseIndex rather than just the string of the Index. That is more versatile
             courses.put(code, new Course(code, courseInd, CourseStatus.WAITLIST));
             courseInd.addToWaitlist(this.matricNumber);
-            return false;
+            return CourseStatus.WAITLIST;
         }
         else{
             //add course successfully
             //TODO Need to modify this to pass in CourseIndex rather than just the string of the Index. That is more versatile
-            courses.put(code, new Course(code, courseInd, CourseStatus.REGISTERED));
+            Course newCourse = new Course(code, courseInd, CourseStatus.REGISTERED)
+            courses.put(code, newCourse);
             courseInd.enrollStudent(this);
-            return true;
+            addAU(newCourse.getAU());
+            return CourseStatus.REGISTERED;
         }
     }
 
@@ -114,6 +135,7 @@ public final class Student extends User {
         
         if (courseStatus == CourseStatus.REGISTERED){
             courseInd.unenrollStudent(this);
+            removeAU(course.getAU());
             this.courses.remove(code);
         }
         else if (courseStatus == CourseStatus.WAITLIST){
@@ -156,10 +178,25 @@ public final class Student extends User {
     checks potential clash with REGISTERED courses
     return true if found a clash
     otherwise, return false
+    Assumes that the code passed in will correspond to
+    a course that has not yet been registered nor waitlisted.
     */
     protected boolean clashes(String code){
         //TODO complete time table clash check
-        return false;
+        boolean doesClash = false;
+        Course c = Database.COURSES.get(code);
+        String[] indices = c.getIndices();
+        //loop through all indices
+        for (String ind : indices){
+            CourseIndex courseIndex = c.getIndex(ind);
+            doesClash = false;
+            //loop through all lessons in courseIndex
+                //TODO check the timings
+            if (!doesClash){ //there is an index that does not clash!
+                return doesClash;
+            }
+        }
+        return doesClash;
     }
 
     /*
@@ -170,9 +207,13 @@ public final class Student extends User {
     */
     protected boolean clashes(String code, String index){
         //TODO complete time table clash check
+
         return false;
     }
 
     //TODO add method that returns Student but the stripped down version to be stored in Courses.
-
+    protected Student simpleCopy(){
+        //what needs to be in the stripped down version?? other than matric number
+        return new Student(this.getUsername(), this.matricNumber);
+    }
 }
