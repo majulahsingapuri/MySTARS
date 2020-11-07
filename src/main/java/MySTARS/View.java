068b0a3aa5db136d6be3ab9c7530725a05bf616a
nicolettehay <nicolettehaysy@gmail.com>
2020-11-08 00:45:46 +0800

    edited LogoutView

--- src/main/java/MySTARS/View.java
+++ src/main/java/MySTARS/View.java
@@ -4,42 +4,8 @@ public abstract class View {
     
     protected abstract void print();
 
-    protected void clearScreen(String directory) {
-        try {
-            final String os = System.getProperty("os.name");
-        
-            if (os.contains("Windows")) {
-                Runtime.getRuntime().exec("cls");
-                }
-            else {
-                Runtime.getRuntime().exec("clear");
-            }
-        }
+    protected void clearScreen(String directory){
 
-        catch (final Exception e) {
-            System.err.println(e);
-        }
-               
-        String spaces = String.format("%" + (74 - directory.length()) + "s", "");
-
-        System.out.println("╔════════════════════════════════════════════════════════════════════════╗");
-        System.out.println("║                                                                        ║");
-        System.out.println("║                              Welcome to                                ║");
-        System.out.println("║                                                                        ║");
-        System.out.println("║      ███╗   ███╗██╗   ██╗███████╗████████╗ █████╗ ██████╗ ███████╗     ║");
-        System.out.println("║      ████╗ ████║╚██╗ ██╔╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝     ║");
-        System.out.println("║      ██╔████╔██║ ╚████╔╝ ███████╗   ██║   ███████║██████╔╝███████╗     ║");
-        System.out.println("║      ██║╚██╔╝██║  ╚██╔╝  ╚════██║   ██║   ██╔══██║██╔══██╗╚════██║     ║");
-        System.out.println("║      ██║ ╚═╝ ██║   ██║   ███████║   ██║   ██║  ██║██║  ██║███████║     ║");
-        System.out.println("║      ╚═╝     ╚═╝   ╚═╝   ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝     ║");                                ║                                                                        ║
-        System.out.println("║                                                                        ║");
-        System.out.println("║                    Nanyang Technological University                    ║");            
-        System.out.println("║                                                                        ║");
-        System.out.println("╚════════════════════════════════════════════════════════════════════════╝");
-        
-        System.out.println("╔════════════════════════════════════════════════════════════════════════╗");
-        System.out.println("║ " + directory + spaces + "║");
-        System.out.println("╚════════════════════════════════════════════════════════════════════════╝");
-        System.out.println();
+        //TODO for Esther/Nicolette: add clear screen print statements. Refer to Tanay's for guidance
     }
 }
