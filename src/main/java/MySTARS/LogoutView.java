5560796e7db2ee4d684dd1da7dd89949c679abae
Bhargav Singapuri <bhsingapuri@gmail.com>
2020-11-09 00:00:28 +0800

    Fixed errors

--- src/main/java/MySTARS/LogoutView.java
+++ src/main/java/MySTARS/LogoutView.java
@@ -7,18 +7,19 @@ public class LogoutView extends View {
     protected void print() {
     clearScreen("Logout");
 
-        Database.CURRENT_USER = null
+        Database.CURRENT_USER = null;
+        Database.CURRENT_ACCESS_LEVEL = AccessLevel.NONE;
            
         System.out.println("╔══════════════════════════════════════════════════╗");
         System.out.println("║                                                  ║");
         System.out.println("║           You have been logged out of            ║");
         System.out.println("║       __  ___     _____________   ___  ____      ║");
-        System.out.println("║      /  |/  /_ __/ __/_  __/ _ | / _ \/ __/      ║");
-        System.out.println("║     / /|_/ / // /\ \  / / / __ |/ , _/\ \        ║");
-        System.out.println("║    /_/  /_/\_, /___/ /_/ /_/ |_/_/|_/___/        ║");
+        System.out.println("║      /  |/  /_ __/ __/_  __/ _ | / _ \\/ __/      ║");
+        System.out.println("║     / /|_/ / // /\\ \\  / / / __ |/ , _/\\ \\        ║");
+        System.out.println("║    /_/  /_/\\_, /___/ /_/ /_/ |_/_/|_/___/        ║");
         System.out.println("║           /___/                                  ║");
         System.out.println("║                                                  ║");
-        System.out.println("╚══════════════════════════════════════════════════╝")                            
+        System.out.println("╚══════════════════════════════════════════════════╝");
 
     }
 
